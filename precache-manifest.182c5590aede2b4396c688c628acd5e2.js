7060b28a49f47fe54ca77b547edd5a90650dedd0
Jovica <jovicabit@gmail.com>
2019-03-12 00:55:28 +0100

    Updates

---
+++ precache-manifest.182c5590aede2b4396c688c628acd5e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "346eb3eec4c1b18bf79d",
+    "url": "/react-gh-pages/static/js/runtime~main.346eb3ee.js"
+  },
+  {
+    "revision": "3dc87d772142ee4370cc",
+    "url": "/react-gh-pages/static/js/main.3dc87d77.chunk.js"
+  },
+  {
+    "revision": "150f7da578ec3c46995c",
+    "url": "/react-gh-pages/static/js/1.150f7da5.chunk.js"
+  },
+  {
+    "revision": "3dc87d772142ee4370cc",
+    "url": "/react-gh-pages/static/css/main.f91c6e0f.chunk.css"
+  },
+  {
+    "revision": "9596ac1ee305498d7cc8fe30196a0496",
+    "url": "/react-gh-pages/index.html"
+  }
+];
